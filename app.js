0c0afb23d64801bfb171034b8b8df3f54a6cdb29
William Cheng <willchengsters@gmail.com>
2019-09-15 16:12:57 -0700

    created express server to serve html files

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000
+const path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.sendFile('./index.html')
+})
+
+app.listen(PORT, () => {
+    console.log("server is listening to:", PORT)
+})
