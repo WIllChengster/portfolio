1edaa1703e272da27ef861fed74ecad202258ae8
burninggun <willchengsters@gmail.com>
2020-06-01 00:08:58 -0700

    commenting email func

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000
 const path = require('path');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+app.use(bodyParser.urlencoded({ extended: false })) 
+
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname + '/')));
 
 app.get('/', (req, res) => {
     res.sendFile('./index.html')
+})
+
+app.post('/api/email', (req, res) => {
+    console.log(req.body.name);
 })
 
 app.listen(PORT, () => {
